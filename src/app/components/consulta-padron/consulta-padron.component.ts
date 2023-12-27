c754e018b83b7c7cb4f533c853e463699d5730ce
miguevaldez79 <37468025+miguevaldez79@users.noreply.github.com>
2023-12-27 08:37:30 -0600

    changes

--- src/app/components/consulta-padron/consulta-padron.component.ts
+++ src/app/components/consulta-padron/consulta-padron.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+
+
+
 @Component({
   selector: 'app-consulta-padron',
   templateUrl: './consulta-padron.component.html',
   styleUrls: ['./consulta-padron.component.scss']
 })
+
+
 export class ConsultaPadronComponent {
+
+
 
 }
